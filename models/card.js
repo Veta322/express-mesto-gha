150197be2f58fae6afce1fe4405b633742ad8661
Veta322 <vetarejh@gmail.com>
2023-04-18 13:01:49 +0300

    200 fix 5

--- models/card.js
+++ models/card.js
@@ -1,30 +1,29 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const cardSchema = new mongoose.Schema({
+const cardSchema = new Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minLength: 2,
+    maxLength: 30,
   },
   link: {
     type: String,
     required: true,
   },
   owner: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
+    ref: 'user',
     required: true,
-    ref: 'user',
   },
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
+  likes: {
+    type: [{ type: Schema.Types.ObjectId, ref: 'user' }],
     default: [],
-    ref: 'user',
-  }],
+  },
   createdAt: {
     type: Date,
     default: Date.now,
   },
 }, { versionKey: false });
 
-module.exports = mongoose.model('card', cardSchema);
+module.exports = model('card', cardSchema);
